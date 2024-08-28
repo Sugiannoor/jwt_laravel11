1b5e14f2ddbecd6df0f1bbed3071bbe25c16908a
Sugiannoor <akhmadsugiannoor9@gmail.com>
2024-08-28 20:47:27 +0800

    Add Middleware Multirole

---
+++ app/Http/Middleware/AuthMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next, ...$roles)
+    {
+        if (!Auth::check()) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+    
+        $user = Auth::user();
+    
+        // Cek apakah user memiliki salah satu dari peran yang diperbolehkan
+        if (!in_array($user->role, $roles)) {
+            return response()->json(['error' => 'Forbidden'], 403);
+        }
+    
+        return $next($request);
+    }
+}
