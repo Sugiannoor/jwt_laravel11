e49b6616f7f9a376a21a0fc59015520d671a631d
Sugiannoor <akhmadsugiannoor9@gmail.com>
2024-08-28 20:47:49 +0800

    Add Router Middleware

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ Route::group([
 ], function ($router) {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login'])->name('login');
-    Route::get('/test', [AuthController::class, 'test']);
+    Route::get('/test', [AuthController::class, 'test'])->middleware(['role:user']);
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
     Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api');
     Route::post('/profile', [AuthController::class, 'profile'])->middleware('auth:api');
